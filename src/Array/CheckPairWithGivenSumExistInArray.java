4df359432fb59b1a6fa5c7bc4c8f325469cf194a
subbu <shawk318@gmail.com>
2021-03-25 20:10:45 +0530

    Check Pair exist in array for Given Sum (25.03.2021)

---
+++ src/Array/CheckPairWithGivenSumExistInArray.java
@@ -0,0 +1,35 @@
+package Array;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class CheckPairWithGivenSumExistInArray {
+
+	public static boolean pairExists(int arr[], int n, int sum) {
+		ArrayList<Integer> al = new ArrayList<>();
+		for (int i = 0; i < n; i++) {
+			int arrEle = sum - arr[i];
+			if (al.contains(arrEle)) {
+				return true;
+			} else {
+				al.add(arr[i]);
+			}
+		}
+		return false;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Please enter size of array");
+		int n = sc.nextInt();
+		int arr[] = new int[n];
+		System.out.println("Please enter array elements");
+		for (int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		System.out.println("Please enter sum value");
+		int sum = sc.nextInt();
+		System.out.println("Pair Exists "+pairExists(arr,n,sum));
+	}
+
+}
